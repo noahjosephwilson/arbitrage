bd58970b56243c5c4dc9f5a1c3a5c8510d0d157c
Noah Wilson <nwilson@bnl.gov>
2025-04-24 18:43:31 -0400

    npm install to admin

---
+++ admin/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "arbitrageadmin74fabef2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
